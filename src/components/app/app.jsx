42e401d530ae462e57fd68335995b10109c9c514
Aleksandr Batollo <a@batollo.ru>
2023-03-21 21:07:41 +0300

    Drafts initial layout

---
+++ src/components/app/app.jsx
@@ -0,0 +1,22 @@
+import { MainPage } from '../main-page/main-page';
+// import { OrderPage } from '../order-page/order-page';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<MainPage />} />
+          {/* <Route path="/order" element={<OrderPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export { App };
